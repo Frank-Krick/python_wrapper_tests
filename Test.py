5402533f05679ac5ad5847fe2ae30044a8a1ced3
Frank Krick <frank.krick@gmail.com>
2015-10-10 22:36:04 -0400

    Test for creating a device graph and connecting devices

--- Test.py
+++ Test.py
@@ -25,16 +25,48 @@ class DeviceGraphAddDevice(unittest.TestCase):
     def test_add_device_to_device_graph(self):
         registry = itk.DeviceRegistry()
         device = registry.registeredDevices()[3]
-        device_graph = itk.DeviceGraph.create()
+        device_graph = itk.DeviceGraph()
         node_id = device_graph.add_device(device)
         self.assertEqual(node_id, 0)
         device = registry.registeredDevices()[6]
         node_id = device_graph.add_device(device)
         self.assertEqual(node_id, 1)
 
+
+class DeviceGraphConnectAudioDevice(unittest.TestCase):
+    def test_connect_audio_devices(self):
+        registry = itk.DeviceRegistry()
+        devices = registry.registeredDevices()
+        audio_devices = [device for device in devices if device.deviceType == itk.DeviceType.Audio]
+        test_device = audio_devices[1]
+        device_graph = itk.DeviceGraph()
+        source = device_graph.add_device(test_device)
+        target = device_graph.add_device(test_device)
+        device_graph.connect(source, target)
+        self.assertTrue(device_graph.is_connected(source, target))
+
+
+class DeviceGraphConnectControlDevice(unittest.TestCase):
+    def test_connect_control_devices(self):
+        registry = itk.DeviceRegistry()
+        devices = registry.registeredDevices()
+        audio_devices = [device for device in devices if device.deviceType == itk.DeviceType.Audio]
+        control_devices = [device for device in devices if device.deviceType == itk.DeviceType.Control]
+        test_control_device = control_devices[0]
+        test_audio_device = audio_devices[0]
+        device_graph = itk.DeviceGraph()
+        source = device_graph.add_device(test_control_device)
+        target = device_graph.add_device(test_audio_device)
+        device_graph.connect(source, target, 0)
+        self.assertTrue(device_graph.is_connected(source, target, 0))
+
+
 testSuite = unittest.TestSuite()
 testSuite.addTest(unittest.makeSuite(DeviceRegistryTest))
 testSuite.addTest(unittest.makeSuite(DeviceTypeTest))
+testSuite.addTest(unittest.makeSuite(DeviceGraphConnectAudioDevice))
+testSuite.addTest(unittest.makeSuite(DeviceGraphConnectControlDevice))
+
 
 if __name__ == "__main__":
     unittest.main()
